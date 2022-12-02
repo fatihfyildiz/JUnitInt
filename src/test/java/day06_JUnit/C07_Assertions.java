f1ed5af30bf1df9511ddb5161659592111ff47f9
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-12-02 13:32:51 +0100

    C07 Assertions

--- src/test/java/day06_JUnit/C07_Assertions.java
+++ src/test/java/day06_JUnit/C07_Assertions.java
@@ -30,37 +30,52 @@ public class C07_Assertions {
          */
         // sayi1'in sayi2'den kucuk oldugunu test edin
         Assert.assertTrue(sayi1<sayi2);
+
         // sayi1'in sayi2'den buyuk olmadigini test edin
         Assert.assertFalse(sayi1>sayi2);
+
         // sayi1'in sayi2'ye esit oldugunu test edin
         Assert.assertEquals(sayi1,sayi2);
     }
     @Test
     public void test02(){
-        Assert.assertEquals(sayi3, sayi1+sayi2); // PASSED
+
+        Assert.assertEquals(sayi3,sayi1+sayi2); // PASSED
     }
+
     @Test
     public void test03(){
-        Assert.assertNotEquals(sayi3, sayi2); // PASSED
+
+        Assert.assertNotEquals(sayi3,sayi2); // PASSED
     }
+
     @Test
     public void test04(){
+
         Assert.assertTrue(sayi3==sayi2); // FAILED
     }
+
     @Test
     public void test05(){
+
         Assert.assertFalse(sayi3==sayi2); // Passed
     }
+
     @Test
     public void test06(){
-        Assert.assertNotEquals(sayi3, sayi1+sayi2); // FAILED
+
+        Assert.assertNotEquals(sayi3,sayi1+sayi2); // FAILED
     }
+
     @Test
     public void test07(){
+
         Assert.assertTrue(sayi3<sayi2); // FAILED
     }
+
     @Test
     public void test08(){
+
         Assert.assertFalse(sayi3>sayi2); // FAILED
         System.out.println("Bu satir yazdirilacak mi ?");
     }
